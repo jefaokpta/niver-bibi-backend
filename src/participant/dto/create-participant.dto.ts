33dd8d05416cf694cfb6a4d338ab38ee55202456
jefaokpta <jefaokpta@hotmail.com>
2024-10-13 19:42:07 -0300

    validadores

--- src/participant/dto/create-participant.dto.ts
+++ src/participant/dto/create-participant.dto.ts
@@ -1,9 +1,17 @@
 import { Guest } from '../entities/guest.entity';
+import { IsNotEmpty, IsNumberString, IsOptional } from 'class-validator';
 
 export class CreateParticipantDto {
+
+  @IsNotEmpty()
   readonly name: string;
+
+  @IsNumberString()
   readonly phone: string;
+
+  @IsOptional()
   readonly isConfirmed: boolean;
-  readonly link: string;
+
+  @IsOptional()
   readonly guests?: [Guest];
 }
