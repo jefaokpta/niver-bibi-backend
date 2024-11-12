78887fae78adc927f3614564585ed863f01f17b5
jefaokpta <jefaokpta@hotmail.com>
2024-11-12 16:57:57 -0300

    pronto pra prod

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,16 +1,10 @@
-import { Controller, Get, Post, Body, Param, Delete, Render } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, BadRequestException } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { CreateAdminDto } from './dto/create-admin.dto';
 
 @Controller('admin')
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
-
-  @Get()
-  @Render('admin')
-  view() {
-    return
-  }
 
   @Post()
   create(@Body() createAdminDto: CreateAdminDto) {
@@ -19,7 +13,8 @@ export class AdminController {
 
   @Post('login')
   login(@Body() login: {password: string}) {
-    return login.password === 'jefao123' ? {isLogged: true} : {isLogged: false};
+    if (login.password === 'jefao123') return
+    throw new BadRequestException('Invalid password');
   }
 
   @Get(':id')
